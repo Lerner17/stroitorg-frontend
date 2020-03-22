b172606cf8591bede39f77aadc0f152976d4bf2b
den503 <zaychuk.dd@gmail.com>
2020-03-22 16:05:29 +0300

    added news card component

---
+++ components/BaseNewsCard.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="content__item">
+    <span class="content__item__date">{{ date }}</span>
+    <img v-if="image" :src="image" />
+    <img v-else src="/images/no_image.png" />
+    <div class="content__item__title">{{ title }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseNewsCard',
+  props: {
+    // eslint-disable-next-line vue/require-default-prop
+    title: String,
+    // eslint-disable-next-line vue/require-default-prop
+    date: String,
+    // eslint-disable-next-line vue/require-default-prop
+    image: String
+  }
+}
+</script>
+
+<style>
+.content__item {
+  display: flex;
+  flex-direction: column;
+  max-width: 400px;
+  width: 100%;
+  height: 365px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
+  margin: 15px;
+}
+.content__item__date {
+  position: absolute;
+  border-radius: 40px;
+  background: orange;
+  color: white;
+  margin: 25px 25px;
+  font-size: 14px;
+  height: 26px;
+  width: 120px;
+  text-align: center;
+  line-height: 25px;
+}
+img {
+  width: 100%;
+  height: auto;
+}
+.content__item__title {
+  padding: 37px 48px;
+  background: #ffffff;
+  align-self: center;
+}
+</style>
--- pages/news/index.vue
+++ pages/news/index.vue
@@ -1,3 +1,57 @@
 <template>
-  <h1>News</h1>
+  <div class="content">
+    <base-news-card
+      v-for="n in news"
+      :key="n.id"
+      :title="n.title"
+      :date="n.date"
+      :image="n.image"
+    ></base-news-card>
+  </div>
 </template>
+
+<script>
+import BaseNewsCard from '@/components/BaseNewsCard'
+export default {
+  components: { BaseNewsCard },
+  data() {
+    return {
+      news: [
+        {
+          id: 1,
+          title: 'Title 1',
+          date: 'Апрель, 22, 2019',
+          image:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRSubSEurzKVzKkQpxewTW_dludtKuIDmciqbeb9OknIwv643u2'
+        },
+        {
+          id: 2,
+          title: 'Title 2',
+          date: 'Май, 17, 2019'
+        },
+        {
+          id: 3,
+          title: 'Title 3',
+          date: 'Июнь, 9, 2019'
+        },
+        {
+          id: 4,
+          title: 'Title 4',
+          date: 'Февраль, 23, 2020'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.content {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding: 20px 100px;
+  width: 100%;
+  box-sizing: border-box;
+}
+</style>
